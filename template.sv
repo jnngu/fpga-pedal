26c6e18d8b8fa4ddba224a2ee5c09d2b8277c69e
ZeddZoo <ziyizuo@andrew.cmu.edu>
2022-02-01 11:30:39 -0500

    template

---
+++ template.sv
@@ -0,0 +1,11 @@
+`default_nettype none
+
+module Template
+(
+  input  logic        clk, reset_n,
+  input  logic [11:0] A,
+  input  logic        update,
+  output logic [11:0] S
+);
+
+endmodule: Template
