d6a2aa63a139876e70d129c5deb8f4fc02440d17
ZeddZoo <ziyizuo@andrew.cmu.edu>
2022-02-01 12:44:16 -0500

    skeleton for top module

---
+++ Top.sv
@@ -0,0 +1,15 @@
+`default_nettype none
+
+/*
+  Top Module for guitar pedal
+    Instantiates all of the internal modules and connects them together
+ */
+
+module Top
+(
+  // TODO: figure out interface signals
+);
+
+
+
+endmodule: Top
