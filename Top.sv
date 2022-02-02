41ed12237a9e2ccd16466cf221db42585a89fede
ZeddZoo <ziyizuo@andrew.cmu.edu>
2022-02-01 23:16:42 -0500

    some more quartus stuffs

--- Top.sv
+++ Top.sv
@@ -7,10 +7,23 @@
 
 module Top
 (
+  input  logic        CLOCK_50
   // TODO: figure out interface signals
-  // what
 );
 
-
+adc_1_channel adc(.CLOCK(CLOCK_50),
+                  .RESET(),
+                  .CH0(),
+                  .CH1(),
+                  .CH2(),
+                  .CH3(),
+                  .CH4(),
+                  .CH5(),
+                  .CH6(),
+                  .CH7(),
+                  .ADC_SCLK(),
+                  .ADC_CS_N(),
+                  .ADC_DOUT(),
+                  .ADC_DIN());
 
 endmodule: Top
--- convolve.sv
+++ convolve.sv
@@ -17,53 +17,54 @@ module Convolve
 );
 
 
-  logic enabled;
-  always_ff @(posedge clk, negedge reset_n)
-    if (~reset_n)
-      enabled <= 1'd0;
-    else if (toggle_en)
-      enabled <= ~enabled;
+  // logic enabled;
+  // always_ff @(posedge clk, negedge reset_n)
+  //   if (~reset_n)
+  //     enabled <= 1'd0;
+  //   else if (toggle_en)
+  //     enabled <= ~enabled;
 
-  logic [DEPTH-1:0][11:0] history, multiplies;
+  // logic [DEPTH-1:0][11:0] history, multiplies;
 
-  genvar i;
-  generate;
-    for (i = 0; i < DEPTH; i++) begin
-      // Create Registers
-      if (i == 0) begin
-        always_ff @(posedge clk, negedge reset_n) begin
-          if (~reset_n)
-            history[i] <= 12'd0;
-          else if (update)
-            history[i] <= new_entry;
-        end
-      end else begin
-        always_ff @(posedge clk, negedge reset_n) begin
-          if (~reset_n)
-            A_history[i] <= 12'd0;
-          else if (update)
-            history[i] <= history[i-1];
-        end
-    end
-  endgenerate
+  // genvar i;
+  // generate
+  //   for (i = 0; i < DEPTH; i++) begin: convolution_history
+  //     // Create Registers
+  //     if (i == 0) begin
+  //       always_ff @(posedge clk, negedge reset_n) begin
+  //         if (~reset_n)
+  //           history[i] <= 12'd0;
+  //         else if (update)
+  //           history[i] <= new_entry;
+  //       end
+  //     end else begin
+  //       always_ff @(posedge clk, negedge reset_n) begin
+  //         if (~reset_n)
+  //           A_history[i] <= 12'd0;
+  //         else if (update)
+  //           history[i] <= history[i-1];
+  //       end
+	// 	end
+	//  end
+  // endgenerate
 
-  /*
-    Compute convolution
-      We compute the convolution incrementally Whenever we update
-      the registers, we add A*B to the history and we subtract the
-      oldest value that we are deprecating. This saves many registers
-      and multiplies as compared to the "dumb" method of keeping
-      an N history for both A and B and recomputing
-   */
-  always_ff @(posedge clk, negedge reset_n) begin
-    if (~reset_n)
-      S <= 12'd0;
-    else if (update)
-      S <= (enabled) ? (conv_result) : A;
-      /* This is technically a cycle late, but it shouldn't really
-         make a big difference. I ***think*** this will be much better
-         for the critical path, however.
-       */
-  end
+  // /*
+  //   Compute convolution
+  //     We compute the convolution incrementally Whenever we update
+  //     the registers, we add A*B to the history and we subtract the
+  //     oldest value that we are deprecating. This saves many registers
+  //     and multiplies as compared to the "dumb" method of keeping
+  //     an N history for both A and B and recomputing
+  //  */
+  // always_ff @(posedge clk, negedge reset_n) begin
+  //   if (~reset_n)
+  //     S <= 12'd0;
+  //   else if (update)
+  //     S <= (enabled) ? (conv_result) : A;
+  //     /* This is technically a cycle late, but it shouldn't really
+  //        make a big difference. I ***think*** this will be much better
+  //        for the critical path, however.
+  //      */
+  // end
 
 endmodule: Convolve
