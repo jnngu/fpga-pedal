14571c93cd83b4b208bbe160b7f34c314ec758c2
ZeddZoo <ziyizuo@andrew.cmu.edu>
2022-02-03 11:06:58 -0500

    fixed some timings and thingies

--- Top.sv
+++ Top.sv
@@ -7,23 +7,51 @@
 
 module Top
 (
-  input  logic        CLOCK_50
+  input  logic        clk, reset_n,
+  input  logic        dout,
+  output logic        cs, din, sclk,
+  output logic  [7:0] LED
+
   // TODO: figure out interface signals
 );
 
-adc_1_channel adc(.CLOCK(CLOCK_50),
-                  .RESET(),
-                  .CH0(),
-                  .CH1(),
-                  .CH2(),
-                  .CH3(),
-                  .CH4(),
-                  .CH5(),
-                  .CH6(),
-                  .CH7(),
-                  .ADC_SCLK(),
-                  .ADC_CS_N(),
-                  .ADC_DOUT(),
-                  .ADC_DIN());
+logic new_clock;
+pll_adc pll1 (.inclk0(clk), .c0(new_clock));
+
+logic [11:0] adc_data, adc_data_reg;
+SPI_interface adc_spi
+  (.clk(new_clock),
+   .reset_b(reset_n), .dout(dout),
+   .ADD(3'd0),
+   .din(din), .sclk(sclk), .cs_b(cs),
+   .data(adc_data));
+
+  logic [31:0] adc_sampler;
+  always_ff @(posedge clk, negedge reset_n) begin
+        if (~reset_n)
+        adc_sampler <= 32'd0;
+      else if (adc_sampler > 32'd1500002)
+        adc_sampler <= 32'd0;
+      else
+        adc_sampler <= adc_sampler + 32'd1;
+  end
+
+  always_ff @(posedge clk, negedge reset_n) begin
+        if (~reset_n)
+            adc_data_reg <= 11'b0;
+      else if (adc_sampler == 32'd1500000)
+        adc_data_reg <= adc_data;
+  end
+
+  assign LED = adc_data_reg[11:4];
+  // genvar i;
+  // generate
+  //   for (i = 0; i < 8; i++) begin: JASSSSOOOOONNNNN
+  //       if (i == 7)
+  //         assign LED[7] = new_clock;
+  //       else
+  //         assign LED[i] = adc_data_reg > (12'b10 << i);
+  //   end
+  // endgenerate
 
 endmodule: Top
--- spi_interface.sv
+++ spi_interface.sv
@@ -1,33 +1,38 @@
-/* 
+/*
  *  SPI interface to work with the DE0_Nano ADC
  *  ADD[3:0] = determines which of the 8 channels to read values from
  *  din, sclk, dout, cs_b: feed to ADC
  *  dout: first four cycles of sclk are 0's, then other 12 cycles are the digital bits of signal, DB11 first then D0
  */
 module SPI_interface (
-    input logic clk, reset_b, dout,
-    input logic[3:0] ADD,
-    output logic din, sclk, cs_b,
-    output logic[11:0] data
+    input logic         clk, reset_b, dout,
+    input logic   [2:0] ADD,
+    output logic        din, sclk, cs_b,
+    output logic [11:0] data
 );
 
     logic [3:0] counter;
 
     always_ff @(posedge clk, negedge reset_b) begin
-        if (~reset_b)
-            counter <= 4'd0;
-        else
-            counter <= counter + 4'd1;
+        if (~reset_b) begin
+          counter <= 4'd0;
+          // cs_b <= 1'b1;
+        end else begin
+          counter <= counter + 4'd1;
+          // cs_b <= 1'b0;
+      end
     end
 
-    assign sclk = cs_b? 1: clk;
+    assign cs_b = (counter == 4'd15) ? 1'd1 : 1'd0;
+
+    assign sclk = cs_b ? 1'b1 : clk;
 
     //might be off by one
     always_comb begin
         case (counter)
-            4'd2: din = ADD[2];
-            4'd3: din = ADD[1];
-            4'd4: din = ADD[0];
+            4'd3: din = ADD[2];
+            4'd4: din = ADD[1];
+            4'd5: din = ADD[0];
             default: din = 1'd0;
         endcase
     end
@@ -36,8 +41,10 @@ module SPI_interface (
         if (~reset_b)
             data <= 11'b0;
         else begin
-            if (counter >= 4'd4)
-                data[counter-4'd4] <= dout;
+            if (counter == 4'd0)
+                data <= 12'd0;
+            else if (counter >= 4'd4)
+                data[4'd11 - counter + 4'd4] <= dout;
         end
 
     end
